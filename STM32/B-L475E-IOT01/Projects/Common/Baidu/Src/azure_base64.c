3649afe7c8064dbcecca7dc077cda3c73552f059
huyun <huandyun@gmail.com>
2018-03-08 13:17:53 +0800

    delete old files

--- STM32/B-L475E-IOT01/Projects/Common/Baidu/Src/azure_base64.c
+++
@@ -1,4 +0,0 @@
-/* Some IDEs complain of the filename clash between mbedTLS and the Azure SDK: The azure file is renamed. */
-#if USE_MBED_TLS
-#include "../../c-utility/src/base64.c"
-#endif /* USE_MBED_TLS */
